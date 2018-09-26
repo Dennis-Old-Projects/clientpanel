a7f3574b751fbeae5bbd9b08d5c682fc5a3c331a
dennis_chacko <dennis_chacko@yahoo.com>
2018-09-25 20:08:01 -0500

    Auth Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import {EditClientComponent} from './components/edit-client/edit-client.componen
 import {ClientDetailsComponent} from './components/client-details/client-details.component';
 import {SettingsComponent} from './components/settings/settings.component';
 import {NotFoundComponent} from './components/not-found/not-found.component';
+import {AuthGuard} from './guards/auth.guard';
 
 //import {environment} from '../environments/environment';
 
@@ -20,13 +21,13 @@ import {NotFoundComponent} from './components/not-found/not-found.component';
 //import {AuthService} from './services/auth.service';
 
 const routes: Routes =[
-  {path: '', component: DashboardComponent},
+  {path: '', component: DashboardComponent,canActivate:[AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'client/add', component: AddClientComponent},
-  {path: 'client/edit/:id', component: EditClientComponent},
-  {path: 'client/:id', component: ClientDetailsComponent},
-  {path: 'settings', component: SettingsComponent},
+  {path: 'client/add', component: AddClientComponent,canActivate:[AuthGuard]},
+  {path: 'client/edit/:id', component: EditClientComponent,canActivate:[AuthGuard]},
+  {path: 'client/:id', component: ClientDetailsComponent,canActivate:[AuthGuard]},
+  {path: 'settings', component: SettingsComponent,canActivate:[AuthGuard]},
   {path: '**', component: NotFoundComponent},
 ];
 @NgModule({
@@ -34,7 +35,7 @@ const routes: Routes =[
   imports: [
     RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AuthGuard],
   declarations: []
 })
 export class AppRoutingModule { }
