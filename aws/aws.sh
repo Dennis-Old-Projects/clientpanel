e308eba045d6bcb4bed2850b24bd9c676c8979b1
Dennis Chacko <dchackda@walgreenco.net>
2019-01-16 14:37:43 -0600

    subnet calculation

--- aws/aws.sh
+++ aws/aws.sh
@@ -1,6 +1,6 @@
 #Create the VPC
 awsresponse=$(aws ec2 create-vpc \
---cidr-block=173.255.221.238/24 \
+--cidr-block=10.0.0.0/16 \
 --output json)
 
 #Extract the VPC id from the response
@@ -14,3 +14,6 @@ echo "Naming the VPC"
 aws ec2 create-tags \
  --resources $vpcId \
  --tags Key=Name,Value=scriptedVp
+
+#Tool to calculated VPC and subnets CIDR
+#http://www.davidc.net/sites/default/subnets/subnets.html?network=172.16.0.0&mask=18&division=7.31
